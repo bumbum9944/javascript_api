3cdf08572475cff277c92c6fda224f0d90188fe5
bumbum94 <tong940526@gmail.com>
2019-11-21 08:55:46 +0900

    파일 백업

---
+++ 01_non_blocking.js
@@ -0,0 +1,4 @@
+// // non-blocking
+// console.log("start")
+// setTimeout(()=>{console.log("wake up")}, 3000)
+// console.log("end")
---
+++ 02_axios.js
@@ -0,0 +1,10 @@
+const axios = require('axios')
+
+axios.get("http://jsonplaceholder.typicode.com/posts")
+    
+    .then((response)=>{
+        console.log(response)
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
